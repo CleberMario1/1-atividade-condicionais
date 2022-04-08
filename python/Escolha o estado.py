f921898abd88773d6b0aa2f036893804ffba188f
Cleber Mario <99451193+CleberMario1@users.noreply.github.com>
2022-04-08 13:47:11 -0300

    Add files via upload

---
+++ python/Escolha o estado.py
@@ -0,0 +1,20 @@
+estados = float(input("Escolha o Estado:\n 1)DF \n 2)TO \n 3)SE \n 4)SP \n 5)SC \n"))
+
+if estados == 1:
+  print("A capital é Brasilia")
+
+elif estados == 2:
+  print("A capital é Palmas")  
+
+elif estados == 3:
+  print("A capital é Aracaju")
+
+elif estados == 4:
+  print("A capital é São Paulo")
+
+elif estados == 5:
+  print("A capital é Florianópolis")
+  
+else:
+  print("Essa opção é inválida")
+print("Fim do programa")  
